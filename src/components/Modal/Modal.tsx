c7f5866539b485abd984c2dba6c2749f93541417
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-27 20:36:43 +0800

    feat: properly type modal props

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,9 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import { HiOutlineX } from "react-icons/hi";
-import { PageContext } from "../../context/PageContext";
+import { usePageContext } from "../../context/PageContext";
+import { twMerge } from "tailwind-merge";
 
-const Modal = ({ children, open, onClose }) => {
-  const { isDarkMode } = useContext(PageContext);
+type ModalProps = {
+  children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+};
+
+const Modal = ({ children, open, onClose }: ModalProps) => {
+  const { isDarkMode } = usePageContext();
 
   useEffect(() => {
     if (open) {
@@ -13,8 +20,8 @@ const Modal = ({ children, open, onClose }) => {
     }
   }, [open]);
 
-  const handleCloseModal = (e) => {
-    const { id } = e.target;
+  const handleCloseModal = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { id } = e.currentTarget;
     if (id === "modal-wrapper") {
       onClose();
     }
@@ -22,23 +29,26 @@ const Modal = ({ children, open, onClose }) => {
   return (
     <div
       onClick={handleCloseModal}
-      className={`bg-black/10 backdrop-filter backdrop-blur-md cursor-pointer ${
-        !open && "scale-0"
-      } fixed inset-0 px-5 overflow-y-auto h-full w-full pt-7 z-50`}
+      className={twMerge(
+        !open && "scale-0",
+        "bg-black/10 backdrop-filter backdrop-blur-md cursor-pointer fixed inset-0 px-5 overflow-y-auto h-full w-full pt-7 z-50"
+      )}
       id="modal-wrapper"
     >
       <div
-        className={`bg-light-blue ${
-          !open && "translate-y-[150vh] opacity-0"
-        } duration-200 ease-in-out translate-y-0 cursor-default w-auto md:w-[45rem] lg:w-[45rem] xl:w-[45rem] 2xl:w-[45rem] h-auto rounded-lg pb-7 my-5 mx-auto`}
+        className={twMerge(
+          !open && "translate-y-[150vh] opacity-0",
+          "bg-light-blue duration-200 ease-in-out translate-y-0 cursor-default w-auto md:w-[45rem] lg:w-[45rem] xl:w-[45rem] 2xl:w-[45rem] h-auto rounded-lg pb-7 my-5 mx-auto"
+        )}
       >
         {children}
       </div>
       <HiOutlineX
         onClick={onClose}
-        className={`${
-          isDarkMode ? "text-white" : "text-blue"
-        } absolute top-3 right-5 cursor-pointer`}
+        className={twMerge(
+          isDarkMode ? "text-white" : "text-blue",
+          "absolute top-3 right-5 cursor-pointer"
+        )}
         size={25}
       />
     </div>
