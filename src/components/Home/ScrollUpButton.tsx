f51037cb8405943ca4ee2e54a059636094f5e738
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-27 20:23:43 +0800

    feat: install tailwind merge

--- src/components/Home/ScrollUpButton.tsx
+++ src/components/Home/ScrollUpButton.tsx
@@ -1,10 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { HiArrowUp } from "react-icons/hi";
-import { PageContext } from "../../context/PageContext";
+import { usePageContext } from "../../context/PageContext";
+import { twMerge } from "tailwind-merge";
 
 const ScrollUpButton = () => {
   const [showScrollUpButton, setShowScrollUpButton] = useState(false);
-  const { isDarkMode, handleScrollToTop } = useContext(PageContext);
+  const { isDarkMode, handleScrollToTop } = usePageContext();
 
   const handleShowScrollUpButton = () => {
     if (window.scrollY >= 60) {
@@ -16,16 +17,18 @@ const ScrollUpButton = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleShowScrollUpButton);
+
+    return () => window.removeEventListener("scroll", handleShowScrollUpButton);
   }, [showScrollUpButton]);
 
   return (
     <div
       onClick={handleScrollToTop}
-      className={`fixed ${
-        isDarkMode ? "bg-white text-blue" : "bg-blue text-white"
-      } ${
-        !showScrollUpButton && "scale-0"
-      }  right-20 bottom-10 duration-300 ease-in-out hover:translate-y-[-1px] cursor-pointer h-10 w-10 rounded-full z-20 flex justify-center items-center`}
+      className={twMerge(
+        isDarkMode ? "bg-white text-blue" : "bg-blue text-white",
+        !showScrollUpButton && "scale-0",
+        "fixed right-20 bottom-10 duration-300 ease-in-out hover:translate-y-[-1px] cursor-pointer h-10 w-10 rounded-full z-20 flex justify-center items-center"
+      )}
     >
       <HiArrowUp size={20} />
     </div>
