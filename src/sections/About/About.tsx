0d9e4a15e7e3ce5cd179f088eeaa6103935b036d
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-06-04 20:58:11 +0800

    feat: create a function for rendering moon element in about page

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -1,14 +1,33 @@
+import { twMerge } from "tailwind-merge";
 import aboutIcon from "../../assets/svg/about.svg";
 import { usePageContext } from "../../context/PageContext";
 const About = () => {
   const { isDarkMode, aboutRef } = usePageContext();
 
+  const renderMoonElement = (moonSize: "small" | "large") => {
+    const isSelectedMoonSizeSmall = moonSize === "small";
+
+    return (
+      <div
+        data-aos="zoom-in"
+        className={twMerge(
+          isDarkMode ? "bg-light-yellow/10" : "bg-blue/10",
+          isSelectedMoonSizeSmall
+            ? "w-20 h-20 -top-8 -left-5"
+            : "w-80 h-80 -top-10 -right-24",
+          "mt-10 md:mt-0 absolute rounded-full"
+        )}
+      />
+    );
+  };
+
   return (
     <div
       ref={aboutRef}
-      className={`${
-        isDarkMode ? "text-white" : "text-black"
-      } page-padding-x max-w-[1640px] w-full mx-auto pt-11 md:pt-38 page-padding-top`}
+      className={twMerge(
+        isDarkMode ? "text-white" : "text-black",
+        "page-padding-x max-w-[1640px] w-full mx-auto pt-11 md:pt-38 page-padding-top"
+      )}
     >
       <p data-aos="fade-right" className="section-title">
         About
@@ -29,18 +48,8 @@ const About = () => {
             an enthusiastic learner, always eager to embrace new skills and
             knowledge that can further enhance my abilities.
           </p>
-          <div
-            data-aos="zoom-in"
-            className={`mt-10 md:mt-0 ${
-              isDarkMode ? "bg-light-yellow/10" : "bg-blue/10"
-            }  w-80 h-80 absolute -top-10 -right-24 rounded-full`}
-          />
-          <div
-            data-aos="zoom-in"
-            className={`mt-10 md:mt-0  ${
-              isDarkMode ? "bg-light-yellow/10" : "bg-blue/10"
-            } h-20 w-20 absolute -top-8 -left-5 rounded-full`}
-          />
+          {renderMoonElement("small")}
+          {renderMoonElement("large")}
         </div>
       </div>
     </div>
